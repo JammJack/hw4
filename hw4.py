c6c24cc434e8bd51f3368cb9b6804fae50629958
JammJack <94935577+JammJack@users.noreply.github.com>
2021-11-23 18:42:51 -0600

    Create hw4.py

---
+++ hw4.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import sys
+import pygame
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+blue = (55,11,171)
+green = (128, 128, 128)
+
+dis_widtth = 800
+dis_height = 600
+
+snake_block = 10
+snake_speed = 10
+
+dis = pygame.display.set_mode((dis_width,dis_height)
+pygame.display.set_caption('snake')
+clock = pygame.time.Clock()
+
+def
